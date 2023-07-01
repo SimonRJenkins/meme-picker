7451583cdec7f419508aade77f38e3965d15a2ee
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-07-01 07:03:40 +0100

    Update index.js
    Removed if statement for catsArray.length > 1 as forgot to remove when testing
    Added a closing > to the img class as I forgot to add one and it was causing the image not to display
    Added an eventlistener to the modal close button so the display of the modal is set to none when clicked
    Sorted out the order of functions

--- index.js
+++ index.js
@@ -5,19 +5,48 @@ const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnly = document.getElementById('gifs-only-option')
 const memeModalInner = document.getElementById('meme-modal-inner')
 const memeModal = document.getElementById('meme-modal')
+const memeCloseButton = document.getElementById('meme-modal-close-btn')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
+
+memeCloseButton.addEventListener('click', closeModal)
 
 getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
-
     for (let radio of radios) {
         radio.classList.remove('highlight')
     }
-
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal() {
+    memeModal.style.display = 'none'
+}
+
+function renderCat() {
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML = `
+    <img 
+    class="cat-img"
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >
+    `
+    memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject() {
+    const catsArray = getMatchingCatsArray()
+
+    if (catsArray.length === 1) {
+        return catsArray[0]
+    }
+    else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
 }
 
 function getMatchingCatsArray() {
@@ -26,6 +55,7 @@ function getMatchingCatsArray() {
         const isGif = gifsOnly.checked;
 
         const matchingCatsArray = catsData.filter(function (cat) {
+
             if (isGif) {
                 return cat.emotionTags.includes(selectedEmotion) && cat.isGif
             }
@@ -37,38 +67,10 @@ function getMatchingCatsArray() {
     }
 }
 
-function getSingleCatObject() {
-    const catsArray = getMatchingCatsArray()
-
-    if (catsArray.length === 1) {
-        return catsArray[0]
-    }
-    else {
-        const randomNumber = Math.floor(Math.random() * catsArray.length)
-        if (catsArray.length > 1) {
-            return catsArray[randomNumber]
-        }
-    }
-}
-
-function renderCat() {
-    const catObject = getSingleCatObject() //temporary
-
-    memeModalInner.innerHTML =
-        `<img 
-    class="cat-img"
-    src="./images/${catObject.image}"
-    alt="${catObject.alt}"
-    `
-    memeModal.style.display = 'flex'
-}
-
 function getEmotionsArray(cats) {
     const emotionsArray = [];
-
     for (let cat of cats) {
         for (let emotion of cat.emotionTags) {
-
             if (!emotionsArray.includes(emotion)) {
                 emotionsArray.push(emotion)
             }
